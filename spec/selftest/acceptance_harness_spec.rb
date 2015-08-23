09b34f50a3bf98743d90582fca21e47038103a32
Zac Bentley <zbbentley@gmail.com>
2015-08-23 11:51:07 -0400

    Fixed previous commit; re-enabled acceptance self tests.

--- spec/selftest/acceptance_harness_spec.rb
+++ spec/selftest/acceptance_harness_spec.rb
@@ -1,27 +1,34 @@
 require "acceptance/spec_helper"
 
-# context "when testing harness" do
-#   tmpfile = Tempfile.new("harness_test")
-#   testpath = tmpfile.path
-#   tmpfile.unlink
+context "when testing harness" do
+  tmpfile = Tempfile.new("harness_test")
+  testpath = tmpfile.path
+  tmpfile.unlink
 
-#   with_manifest("file { '#{testpath}': ensure => 'present' }", "single-file") do
-#     it "creates expected file" do
-#       expect(file(testpath)).to be_file
-#     end
-#   end
+  # If you don't have a homebrew-installed stat, serverspec's matchers won't
+  # work.
+  it "system has a usable stat" do
+  	expect(command("stat -c %F " + __FILE__).exit_status).to be_zero
+  	expect(file("/dev/null").size).not_to be_nil
+  end
 
-#   with_manifest("notice(1)", "non-state-changing", :expect_changes => false) {}
-#   with_manifest(
-#     "fail('#{testpath}')",
-#     "compilation-breaking",
-#     :expect_changes => false,
-#     :compile_failure => testpath, # Compilation failure should contain test file path.
-#   ) {}
-#   with_manifest(
-#     "file { '/dev/null/#{testpath}': ensure => 'present' }",
-#     "apply-failing",
-#     :apply_failure => testpath,
-#     :expect_changes => false
-#   ) {}
-# end
+  with_manifest("file { '#{testpath}': ensure => 'present' }", "single-file") do
+    it "creates expected file" do
+      expect(file(testpath)).to be_file
+    end
+  end
+
+  with_manifest("notice(1)", "non-state-changing", :expect_changes => false) {}
+  with_manifest(
+    "fail('#{testpath}')",
+    "compilation-breaking",
+    :expect_changes => false,
+    :compile_failure => testpath, # Compilation failure should contain test file path.
+  ) {}
+  with_manifest(
+    "file { '/dev/null/#{testpath}': ensure => 'present' }",
+    "apply-failing",
+    :apply_failure => testpath,
+    :expect_changes => false
+  ) {}
+end
